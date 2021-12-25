3a0eb9243d5333cc0a75a208f36b4304a95baef7
MrMayami <87673173+MrMayami@users.noreply.github.com>
2021-12-26 00:54:44 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,23 @@ from rave_python import Rave
 from flask import Flask, request
 import json
 import sys
+import pyrebase
 
 app = Flask(__name__)
+
+firebaseConfig = {
+    'apiKey': "AIzaSyDySGSLHwwPI_ouMYFaSlG53YEz_U5VVH4",
+    'authDomain': "splishpay-tga.firebaseapp.com",
+    'projectId': "splishpay-tga",
+    'storageBucket': "splishpay-tga.appspot.com",
+    'messagingSenderId': "989782550449",
+    'appId': "1:989782550449:web:ce0df59b2fddf4d7d8ff90",
+    'measurementId': "G-1TTHTEQG8M"}
+
+firebase = pyrebase.initialize_app(firebaseConfig)
+mAuth = firebase.auth()
+
+# storage=firebase.storage()
 
 
 @app.route('/pay', methods=['POST'])
@@ -22,3 +37,31 @@ def pay():
     res = rave.Card.charge(payload)
 
     return str(res['txRef'])
+
+
+@app.route('/login', method=['POST'])
+def login():
+    try:
+        mEmail = "pr.mayami@gmail.com"
+        mPassword = "footBall@2020"
+        res = mAuth.sign_in_with_email_and_password(mEmail, mPassword)
+    except Exception as e:
+        print("An error occured: " + str(e))
+
+        return e
+
+    return res
+
+
+@app.route('/signup', method=['POST'])
+def signup():
+    try:
+        mEmail = "pr.mayami@gmail.com"
+        mPassword = "footBall@2020"
+        res = mAuth.create_user_with_email_and_password(mEmail, mPassword)
+    except Exception as e:
+        print("An error occured: " + str(e))
+
+        return e
+
+    return res
